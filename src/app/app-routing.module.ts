9755c8fe7f81440b38d47cf9143c45cd717ff7f5
Utkarsh Raj <utkraj@publicisgroupe.net>
2021-11-21 12:55:14 +0530

    Routing without lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RouteAComponent } from './feature/route-a/route-a.component';
 import { RouteBComponent } from './feature/route-b/route-b.component';
+import { RouteCComponent } from './feature/route-c/route-c.component';
+import { RouteDComponent } from './feature/route-d/route-d.component';
+import { RouteEComponent } from './feature/route-e/route-e.component';
+import { RouteFComponent } from './feature/route-f/route-f.component';
 
 const routes: Routes = [
   { path: 'route-a', component: RouteAComponent },
-  { path: 'route-b', component: RouteBComponent }
+  { path: 'route-b', component: RouteBComponent },
+  { path: 'route-c', component: RouteCComponent },
+  { path: 'route-d', component: RouteDComponent },
+  { path: 'route-e', component: RouteEComponent },
+  { path: 'route-f', component: RouteFComponent }
 ];
 
 @NgModule({
